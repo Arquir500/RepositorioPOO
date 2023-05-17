fdd0cdf2166e90fd4374ffe99870f0e3fb1f2a09
Renan Nunes <renan_nunesdasilva12@hotmail.com>
2023-05-17 18:25:09 -0300

    aula02teste1

---
+++ src/main/java/com/mycompany/aula02/Aula02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula02;
+
+/**
+ *
+ * @author Renan
+ */
+public class Aula02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
