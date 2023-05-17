e1e1adfa335a959efb893466ecaa24edd083a338
Renan Nunes <renan_nunesdasilva12@hotmail.com>
2023-05-17 18:59:52 -0300

    Entendi a logica

--- src/main/java/com/mycompany/aula02/Aula02.java
+++ src/main/java/com/mycompany/aula02/Aula02.java
@@ -5,7 +5,24 @@ package com.mycompany.aula02;
 public class Aula02 {
 
     public static void main(String[] args) {
-        System.out.println("Olá, Mundo! um dois três");
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.modelo = "Bic";
+        c1.carga = 85;
+        c1.destampar();
+        c1.status();
+        c1.rabiscar();
+        
+        Caneta c2 = new Caneta();
+        c2.modelo = "Fox";
+        c2.cor = "Prata";
+        c2.carga = 100;
+        c2.ponta = 1.0f;
+        c2.tampar();
+        c2.status();
+        c2.rabiscar();
+        
         
     }
 }
