722cd1a5d75737ef87d11280c44995cc05ccb3af
Renan Nunes <renan_nunesdasilva12@hotmail.com>
2023-05-17 18:26:53 -0300

    aula02teste2

--- src/main/java/com/mycompany/aula02/Aula02.java
+++ src/main/java/com/mycompany/aula02/Aula02.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.aula02;
 
-/**
- *
- * @author Renan
- */
+
 public class Aula02 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Olá, Mundo!");
     }
 }
