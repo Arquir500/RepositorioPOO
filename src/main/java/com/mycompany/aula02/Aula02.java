75de976e6676eb738d3a6be1bdc71b32d14ed9f1
Renan Nunes <renan_nunesdasilva12@hotmail.com>
2023-05-22 22:18:34 -0300

    Configurando Visibilidade de Atributos e Métodos

--- src/main/java/com/mycompany/aula02/Aula02.java
+++ src/main/java/com/mycompany/aula02/Aula02.java
@@ -1,4 +1,4 @@
-
+/*
 package com.mycompany.aula02;
 
 
---
+++ src/main/java/com/mycompany/aula02/Aula03.java
@@ -0,0 +1,23 @@
+package com.mycompany.aula02;
+
+
+public class Aula03 {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "Verde";
+        c1.modelo = "Bic Lemon";
+        //c1.ponta = 0.3f;
+        c1.carga = 75;
+        //c1.tampada = true;
+        c1.destampar();
+        c1.status();
+        c1.rabiscar();
+        
+        
+    
+        
+        
+    }
+}
+
